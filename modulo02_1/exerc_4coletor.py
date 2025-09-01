c5bbc7d6093e654a36e711a2e820ebc53970bfe3
JherikaSilva <pereirajherika@gmail.com>
2025-09-01 15:54:37 -0300

    exercicio 4

---
+++ modulo02_1/exerc_4coletor.py
@@ -0,0 +1,5 @@
+"""Coletor de dados Robusto
+Crie um programa que colete numeros de usuario e os armazene em uma lista, o programa deve continuar pedindo numeros ate que o usuario digite -1 para parar
+Ele deve validar a entrada para garantir que o que foi digitado é realmente um numero antes de prosseguir, apenas os numeros entre 0 e 100 devem ser consideradoa validos e adicionados a lista
+ao final imprima a soma dos numeros validos e a lista de numeros coletados"""
+ 
