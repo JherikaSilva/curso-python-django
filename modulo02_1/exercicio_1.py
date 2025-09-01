e9d1a84545c3b42a44e9efc006d756e1c39dd212
JherikaSilva <pereirajherika@gmail.com>
2025-09-01 14:08:54 -0300

    conta os numeros

---
+++ modulo02_1/exercicio_1.py
@@ -0,0 +1,14 @@
+#Crie um programa que conte quantas vezes um numero especifico aparece em uma lista
+
+numeros=[1,6,7,9,6,3,6,6]
+resultado= numeros.count(6)
+print(f"A quantidade de vezes que o numero 6 aparece e {resultado}")
+
+"""Outra forma
+numeros=[1,6,7,9,6,3,6,6]
+numero_proc=6
+contador=0
+for numero in numeros:
+    if numero==numero_proc:
+        contador+=1
+print(f"O {numero_proc} aparece {contador} vezes!")"""       
