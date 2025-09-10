8986ccf09b4d4bb135fa48ebd2de92bb5e227aa5
JherikaSilva <pereirajherika@gmail.com>
2025-09-10 14:01:26 -0300

    definanado funcoes

---
+++ orientacao_objetos/funcao.py
@@ -0,0 +1,18 @@
+#def diga_oi():
+ #   print("Oi! Tudo bem?")
+#diga_oi()    
+#def saudar(nome):
+ #   print(f"Ola, {nome}")
+#saudar("Maria")  
+
+#def encontrar_par(lista):
+#    for numero in lista:
+#        if numero %2==0:
+#            return numero
+#    return None
+
+#print(encontrar_par([1,3,5,6,8]))
+
+#dobrar= lambda x: x*2
+#print(dobrar(5))
+
