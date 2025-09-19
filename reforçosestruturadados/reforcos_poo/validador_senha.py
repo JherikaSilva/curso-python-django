c5db7864c138b8f235cf56d8b9bae48120833c4e
JherikaSilva <pereirajherika@gmail.com>
2025-09-19 10:49:54 -0300

    alterei o if para fora da função

--- reforçosestruturadados/reforcos_poo/validador_senha.py
+++ reforçosestruturadados/reforcos_poo/validador_senha.py
@@ -1,13 +1,14 @@
 """3. Validador de Senha: Crie uma função que receba uma senha (string) e retorne True se
 ela tiver pelo menos 8 caracteres e False caso contrário"""
 
-def validador(senha:str)-> bool:
-    if len(senha)>=8:
-        return True
-    
-    else:
-        return False
-    
+def validador(senha:str)->bool:
+    """Valida se a senha tem pelo menos 8 caracteres"""
+    return len(senha)>=8
     
 senha=input("Digite sua senha: ")
-validador(senha)        
+if validador(senha):  
+     print("Senha válida")
+else:
+    print("Senha inválida")
+    
+               
