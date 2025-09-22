f3f6fd58a1337c55ec23debc72d215607193415f
JherikaSilva <pereirajherika@gmail.com>
2025-09-22 13:08:16 -0300

    Movi a pasta e alterei o nome

--- Aula-04-programa-o-Python/caça_palavras.py
+++
@@ -1,8 +0,0 @@
-#Peça ao usuário para digitar uma frase. Conte quantas vezes a palavra "a" aparece na frase (ignorando maiúsculas e minúsculas).
-print("------ CAÇADOR DE PALAVRAS -----")
-print("Vamos contar quantas vezes a letra 'A' aparece na sua frase")
-letra_a= input("Digite sua frase: ").lower()
-if "a" in letra_a:
-    print("A letra 'A' aparece: ", letra_a.count("a"), "vezes!")
-else:
-    print("A frase não contém letra 'A' !")    
