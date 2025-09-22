8c13469bbbd96fc7a614de46017c6fa350360e65
JherikaSilva <pereirajherika@gmail.com>
2025-09-22 13:10:20 -0300

    movi a pasta de local

---
+++ Inicio_em_Python/for-python/maior.py
@@ -0,0 +1,6 @@
+maior=float()
+for i in range(5):
+    num=int(input("Digite um número: "))
+    if num > maior:
+        maior=num
+print("O maior número é: ", maior)        
---
+++ Inicio_em_Python/for-python/tabuadasimples.py
@@ -0,0 +1,6 @@
+print("--------- TABUADA SIMPLES ---------")
+numero=int(input("Digite um número inteiro: "))
+
+for tabuada in range(1,11):
+    tabuada*numero
+    print("A tabuada do número é: ",tabuada*numero)
