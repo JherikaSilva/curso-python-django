649bf71ca2f02213f493bda15789518a1827a959
Jherika Silva <pereirajherika@gmail.com>
2025-09-22 17:15:08 -0300

    removi um comentario e alterei a pasta

--- exercicios-de-reforco-curso-python/reforçosestruturadados/reforcos_poo/exerc_classes/produto.py
+++ exercicios-de-reforco-curso-python/reforçosestruturadados/reforcos_poo/exerc_classes/produto.py
@@ -1,8 +1,3 @@
-"""Crie uma nova classe chamada Produto. Todo produto deve ter nome e preço. Depois, crie
-duas instâncias:
-1. Um "Caderno" que custa 15.50.
-2. Uma "Caneta" que custa 3.00.
-Imprima o nome e o preço de cada produto"""
 
 class Produto:
     def __init__(self, nome, preco):
