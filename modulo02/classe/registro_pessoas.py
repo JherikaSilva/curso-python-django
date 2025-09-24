25f29cd798b554f359172473425d5b5ac6e47506
JherikaSilva <pereirajherika@gmail.com>
2025-09-24 13:50:14 -0300

    classe e superclasse

---
+++ modulo02/classe/registro_pessoas.py
@@ -0,0 +1,27 @@
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome=nome
+        self.idade=idade
+        
+    def apresentar(self):
+        print(f"Nome: {self.nome} \nIdade: {self.idade}\n")
+        
+
+class Estudante(Pessoa):
+    def __init__(self, nome, idade, curso):
+        super().__init__(nome, idade)            
+        self.curso=curso
+        
+    def apresentar(self):
+           print(f"Nome: {self.nome} \nIdade: {self.idade} \nCurso: {self.curso}\n")
+
+
+
+p_1=Pessoa("Marta", 25)
+est_1=Estudante("Jherika", 27, "Python")
+
+lista=[p_1, est_1]
+
+for pess in lista:
+    pess.apresentar()
