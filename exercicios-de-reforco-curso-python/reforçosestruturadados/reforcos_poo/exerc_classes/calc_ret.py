49ed7e84bda44b81afac2bc7f10f2570a8c3d0d5
JherikaSilva <pereirajherika@gmail.com>
2025-10-01 13:11:25 -0300

    exclui um comentario

--- exercicios-de-reforco-curso-python/reforçosestruturadados/reforcos_poo/exerc_classes/calc_ret.py
+++ exercicios-de-reforco-curso-python/reforçosestruturadados/reforcos_poo/exerc_classes/calc_ret.py
@@ -1,7 +1,4 @@
-"""Crie uma classe Retangulo que é inicializada com base e altura. Crie dois métodos:
-1. calcular_area(): deve retornar o cálculo base * altura.
-2. calcular_perimetro(): deve retornar o cálculo 2 * (base + altura).
-Crie um retângulo, chame os dois métodos e imprima os resultados que eles retornam."""
+
 
 class Retangulo:
     def __init__(self, base,altura):
--- exercicios-de-reforco-curso-python/reforçosestruturadados/reforcos_poo/exerc_classes/conta_bancaria.py
+++ exercicios-de-reforco-curso-python/reforçosestruturadados/reforcos_poo/exerc_classes/conta_bancaria.py
@@ -1,7 +1,4 @@
-"""Crie uma classe ContaBancaria. Toda conta deve começar com um titular e um saldo inicial.
-Crie um método depositar(valor) que some um valor ao saldo da conta. Crie um objeto,
-deposite um valor e imprima o novo saldo.
-"""
+
 
 class ContaBancaria:
     def __init__(self, titular, saldo_inicial):
