be220cfe4db3b06552c1aa1f1d2a687b40547fea
JherikaSilva <pereirajherika@gmail.com>
2025-10-01 13:10:14 -0300

    exclui um comentario

--- modulo02/desafios/super_calc.py
+++ modulo02/desafios/super_calc.py
@@ -1,10 +1,4 @@
-"""Crie um programa que funcione como uma calculadora de bolso. Ele deve ser capaz de fazer
-adição, subtração, multiplicação e divisão.
-O programa deve sempre mostrar um menu de opções, pedir ao usuário para escolher a
-operação e digitar dois números. No final, ele exibe o resultado da conta. Se houver algum
-erro, como uma divisão por zero ou o usuário digitar algo que não é um número, o programa
-deve avisar e não travar.
-"""
+
 def somar(primeiro_num:int, segundo_num:int) ->int:
     """Função para somar números"""
     return primeiro_num + segundo_num
