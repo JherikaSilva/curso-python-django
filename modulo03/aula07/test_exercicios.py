ea181c10fd890c340889f545f032daff13e09d28
Jherika Silva <pereirajherika@gmail.com>
2025-10-27 17:51:24 -0300

    testes dos exercicios atualizados

--- modulo03/aula07/test_exercicios.py
+++ modulo03/aula07/test_exercicios.py
@@ -1,12 +1,44 @@
 from exercicios import Inventario
 import pytest
 
-def test_adicionar_produto():
+def test_adicionar_produto_novo():
     adc=Inventario()
-
-    resultado_obtido= adc.adicionar_produto("Martelo", 10)
-    resultado_esperado= [{"Martelo":10}]
+    adc.adicionar_produto("Martelo", 10)
+    adc.adicionar_produto("Faca", 20)
+    resultado_obtido= adc.lista_estoque
+    resultado_esperado= [{"produto": "Martelo", "quantidade": 10},{"produto":"Faca", "quantidade":20}]
 
     assert resultado_obtido== resultado_esperado
 
+def adicionar_produto_existente():
+    adc=Inventario()
+    adc.adicionar_produto("Martelo", 10)
+    adc.adicionar_produto("Martelo", 20)
+    adc.adicionar_produto("Martelo", 10)
+    
+    resultado_obtido= adc.lista_estoque
+    resultado_esperado= [{"produto": "Martelo", "quantidade": 40}]
+    assert resultado_obtido== resultado_esperado
 
+
+def test_remover_produto():
+    rem=Inventario()
+    rem.adicionar_produto("Martelo", 10)
+    rem.adicionar_produto("Faca", 20)
+
+    rem.remover_produto("Martelo")  
+
+    resultado_obtido= rem.lista_estoque
+    resultado_esperado= [{"produto":"Faca", "quantidade":20}]
+
+    assert resultado_obtido== resultado_esperado
+
+def test_remover_produto_ValueError():
+    rem=Inventario()
+    rem.adicionar_produto("Martelo", 10)
+    rem.adicionar_produto("Faca", 20)
+
+    rem.remover_produto("Relogio")
+    with pytest.raises(ValueError) as r: 
+        rem.remover_produto("Relogio")
+        assert "Produto não encontrado"  in str(r.value) 
