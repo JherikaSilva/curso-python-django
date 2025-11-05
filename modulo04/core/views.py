c2a0503e32b006465b154d3a6f9142e6e4e363bb
Stefane-7 <pereirajherika@gmail.com>
2025-11-05 15:30:19 -0300

    alterei opções

--- modulo04/core/views.py
+++ modulo04/core/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def home(request):
+    context={
+        'nome_usuario':'Jherika',
+        'tecnologias': ['Python', 'Django', 'HTML','CSS']
+    }
+    
+    return render(request, 'home.html', context)
 
-    return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
 
 def ola(request):
     
