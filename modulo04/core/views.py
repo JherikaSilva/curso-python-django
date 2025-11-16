7a965691505c161ef931a46efdeb0a3f1c4f2d73
Jherika Silva <pereirajherika@gmail.com>
2025-11-16 18:07:22 -0300

    importei o redirect

--- modulo04/core/views.py
+++ modulo04/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from.models import Tarefa
 from .forms import TarefaForm 
@@ -8,15 +8,15 @@ def home(request):
     if request.method== 'POST':
         
         form= TarefaForm(request.POST)
-
-    if form.is_valid():
         
-        form.save()
-
-        return redirect('home')
+        if form.is_valid():
+            
+            form.save()
+            
+            return redirect('home')
     
     else:
-
+        
         form=TarefaForm()
 
 
