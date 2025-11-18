e693cdebd41cf74222cc9065d4da52718a489b1f
Jherika Silva <pereirajherika@gmail.com>
2025-11-18 14:08:38 -0300

    adcionei urls para deletar e adicionar

--- modulo04/core/urls.py
+++ modulo04/core/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('ola/', views.ola, name='ola'),
+    path('tarefa/<int:pk>/concluir/', views.concluir_tarefa, name='concluir_tarefa'),
+    path('tarefa/<int:pk>/deletar/', views.deletar_tarefa, name='deletar_tarefa'),
 ]
