0352d2cbf9317ec0404464970a4faeca72b4c26a
JherikaSilva <pereirajherika@gmail.com>
2025-11-19 15:07:55 -0300

    exclui path ola e adicionei register

--- modulo04/core/views.py
+++ modulo04/core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .models import Tarefa
+from .forms import TarefaForm
 
-from django.http import HttpResponse
-from.models import Tarefa
-from .forms import TarefaForm 
-
+@login_required
 def home(request):
 
     if request.method== 'POST':
@@ -11,8 +13,10 @@ def home(request):
         form= TarefaForm(request.POST)
         
         if form.is_valid():
-            
-            form.save()
+
+            tarefa = form.save(commit=False) 
+            tarefa.user = request.user 
+            tarefa.save()
             
             return redirect('home')
     
@@ -22,38 +26,51 @@ def home(request):
 
 
     
-    todas_as_tarefas=Tarefa.objects.all().order_by('-criada_em')
+    todas_as_tarefas=Tarefa.objects.filter(user=request.user).order_by('-criada_em')
 
     context={
-        'nome_usuario':'Jherika',
-        'tecnologias': ['Python', 'Django', 'HTML', 'CSS','Models','Admin'],
+        'nome_usuario':request.user.username,
+        'tecnologias': ['Autenticação','ForeignKey','Login'],
         'tarefas': todas_as_tarefas,
         'form': form,
     }
     
     return render(request, 'home.html', context)
 
-
-def ola(request):
-    
-    return HttpResponse("<h1>Bem vindo!</h1><p><input>LOGIN</input><br></h1><input>SENHA</input></p>")
-
+@login_required
 def concluir_tarefa(request, pk):
 
-    tarefa = get_object_or_404(Tarefa, pk=pk)
+    tarefa = get_object_or_404(Tarefa, pk=pk, user=request.user)
 
     if request.method == 'POST':
 
         tarefa.concluida = True
         tarefa.save()
         return redirect('home')
-    
+
+@login_required    
 def deletar_tarefa(request, pk):
 
-    tarefa = get_object_or_404(Tarefa, pk=pk)
+    tarefa = get_object_or_404(Tarefa, pk=pk,user=request.user)
 
     if request.method == 'POST':
 
         tarefa.delete()
         return redirect('home')
 
+def register(request): 
+
+    if request.method == 'POST': 
+
+        form = UserCreationForm(request.POST) 
+
+        if form.is_valid(): 
+            user = form.save() 
+            login(request, user) 
+            return redirect('home') 
+ 
+    else: 
+        form = UserCreationForm() 
+
+    context = {'form': form}
+    return render(request, 'register.html', context)
