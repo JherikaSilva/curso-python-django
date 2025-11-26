26301a24f1f59af51bcef3636de4440de4192fbf
Jherika Silva <pereirajherika@gmail.com>
2025-11-26 20:07:14 -0300

    inclui urls de projects

--- modulo04/meuprojeto/urls.py
+++ modulo04/meuprojeto/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('core/', include('core.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'),name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'),name='logout'),
+    path('projects/', include('projects.urls')),
 
 ]
