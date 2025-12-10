bdbfd35c019e31cf628074f98faee6242022b407
paulobarreto27 <pereirajherika@gmail.com>
2025-12-10 15:25:27 -0300

    add url tarefa

--- modulo05/core/urls.py
+++ modulo05/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ListaTarefasAPIView
+from .views import ListaTarefasAPIView, DetalheTarefaAPIView
 
 app_name= 'core'
 
 urlpatterns= [
     path('tarefas/', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
+    path('tarefas/<int:pk>/', DetalheTarefaAPIView.as_view(), name='detalhe-tarefa')
     
 ]
