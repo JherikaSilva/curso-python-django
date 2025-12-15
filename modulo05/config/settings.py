35f1e44cf943de68a450f48eb102787269e5c84c
JherikaSilva <pereirajherika@gmail.com>
2025-12-15 15:35:22 -0300

    feat: adicionei apps e importacoes

--- modulo05/config/settings.py
+++ modulo05/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 import environ
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'core',
+    'rest_framework_simplejwt.token_blacklist'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,24 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+    # Define JWT como método de autenticação PADRÃO
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    # Outras configurações padrão
+}
+
+SIMPLE_JWT = {
+    # Tempo de vida do Access Token (curto!)
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    # Tempo de vida do Refresh Token (longo!)
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    # Define o esquema de autenticação no header HTTP
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    # Algoritmo de criptografia
+    'ALGORITHM': 'HS256',
+    # Nome do campo de usuário no payload (user_id é padrão)
+    'USER_ID_CLAIM': 'user_id',
+}
