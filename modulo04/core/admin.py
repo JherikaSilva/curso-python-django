2d04c0ed442b3b3f411d884266ce0df553af1e16
Jherika Silva <pereirajherika@gmail.com>
2025-12-16 19:24:12 -0300

    adiconei ao display descricao,prazo e prioridade

--- modulo04/core/admin.py
+++ modulo04/core/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import Tarefa
 
 class TarefaAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'user','get_user_email', 'concluida', 'criada_em','project')
-    list_filter = ('concluida', 'user', 'criada_em','project')
-    search_fields = ('titulo', 'user__username','project')
+    list_display = ('titulo', 'user','get_user_email', 'concluida', 'criada_em','project', 'prioridade',
+        'prazo','data_conclusao',)
+    list_filter = ('concluida', 'user', 'criada_em','project','prioridade', 'concluida')
+    search_fields = ('titulo', 'user__username','project','descricao')
     fieldsets = (
         ('Informações Principais', {
             'fields': ('user','project', 'titulo')
