6843d71db29b0d96c99748a3c827657bd8e7ea25
Jherika Silva <pereirajherika@gmail.com>
2025-12-16 19:25:34 -0300

    adicionei urls de contagem, e duplicar tarefas

--- modulo04/core/urls.py
+++ modulo04/core/urls.py
@@ -1,10 +1,14 @@
 # core/urls.py
 from django.urls import path
-from . import views 
+from . import views
+from .views import contagem_tarefas,duplicar_tarefa
 
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('tarefa/<int:pk>/concluir/', views.concluir_tarefa, name='concluir_tarefa'),
     path('tarefa/<int:pk>/deletar/', views.deletar_tarefa, name='deletar_tarefa'),
     path('register/', views.register, name='register'),
+    path('tarefas/contagem/', contagem_tarefas, name='contagem-tarefas'),
+    path('tarefas/<int:tarefa_id>/duplicar/', duplicar_tarefa, name='duplicar_tarefa'),
 ]
+
